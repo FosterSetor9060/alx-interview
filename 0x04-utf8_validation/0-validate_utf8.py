cc3dd11b170cee3a414a559600850997a20829d9
Foster Setor <walkerdziwornu@gmail.com>
2024-08-23 00:18:30 -0700

    myTasKForUtf8Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-"""UTF-8 validator"""
+"""UTF-8 validation module"""
 
 
 def validUTF8(data):
     """
-    Check if a given list of integers represents a valid UTF-8 encoding.
+    Validate if a list of integers represents valid UTF-8 encoded data.
     
     Args:
-        data (list): A list of integers representing the data to validate.
+        data (list): A list of integers representing the data to be validated.
         
     Returns:
         bool: True if the data is a valid UTF-8 encoding, False otherwise.
@@ -15,19 +15,18 @@ def validUTF8(data):
     num_bytes = 0
 
     for byte in data:
-        binary_rep = format(byte, '#010b')[-8:]
+        bin_rep = format(byte, '08b')
 
         if num_bytes == 0:
-            if binary_rep[0] == '1':
-                num_bytes = len(binary_rep.split('0')[0])
-
+            if bin_rep[0] == '1':
+                num_bytes = len(bin_rep.split('0')[0])
             if num_bytes == 0:
                 continue
 
             if num_bytes == 1 or num_bytes > 4:
                 return False
         else:
-            if not (binary_rep[0:2] == '10'):
+            if not (bin_rep[0:2] == '10'):
                 return False
 
         num_bytes -= 1
