94945d953f12d9dbce2e386927541fd23d50da54
Foster Setor <walkerdziwornu@gmail.com>
2024-09-04 13:54:42 -0700

    myStarWarsApi_task

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,26 +1,17 @@
 #!/usr/bin/node
 
 const request = require('request');
-const movieId = process.argv[2];  // First positional argument is the movie ID
-const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;  // SWAPI URL with movie ID
 
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-    return;
-  }
-
-  const filmData = JSON.parse(body);
-  const characters = filmData.characters;  // Array of character URLs
-
-  characters.forEach((characterUrl) => {
-    request(characterUrl, (err, res, charBody) => {
-      if (err) {
-        console.error('Error:', err);
-        return;
-      }
-      const characterData = JSON.parse(charBody);
-      console.log(characterData.name);  // Print character name
-    });
-  });
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
+  if (err) throw err;
+  const actors = JSON.parse(body).characters;
+  exactOrder(actors, 0);
 });
+const exactOrder = (actors, x) => {
+  if (x === actors.length) return;
+  request(actors[x], function (err, res, body) {
+    if (err) throw err;
+    console.log(JSON.parse(body).name);
+    exactOrder(actors, x + 1);
+  });
+};
